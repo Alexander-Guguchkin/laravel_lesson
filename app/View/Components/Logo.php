11d5eed7b19bf5a98fb6048de2a08da5454ee44b
Alexander <guguckins72@gmail.com>
2023-11-29 16:09:17 +0300

    new v6

--- app/View/Components/Logo.php
+++ app/View/Components/Logo.php
@@ -8,24 +8,12 @@ use Illuminate\View\Component;
 
 class Logo extends Component
 {
-<<<<<<< HEAD
     /**
      * Create a new component instance.
      */
     public function __construct()
     {
 
-=======
-    public $src;
-    public $alt;
-    /**
-     * Create a new component instance.
-     */
-    public function __construct($src, $alt)
-    {
-        $this->src = $src;
-        $this->alt = $alt;
->>>>>>> e721b3a1765057709b11256179692000038f04c9
     }
 
     /**
@@ -33,10 +21,6 @@ class Logo extends Component
      */
     public function render(): View|Closure|string
     {
-<<<<<<< HEAD
         return view('components.logo');
-=======
-        return view('components.logo', ['src' => $this->src, 'alt' => $this->alt]);
->>>>>>> e721b3a1765057709b11256179692000038f04c9
     }
 }
--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
 <div>
     <img {{$attributes}} />
-=======
-<div class="logo">
-    {{$alt}}
-    {{-- {{$title}} --}}
-    {{$src}}
-    <img src="{{$src}}" alt="{{$alt}}"  style = "width: 250px; height: 250px;">
->>>>>>> e721b3a1765057709b11256179692000038f04c9
 </div>
