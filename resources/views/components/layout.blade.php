8ca40fffdd6fae7674764564dff9baf7dc6cdf84
Alexander <guguckins72@gmail.com>
2023-11-29 16:03:33 +0300

    new v6

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,6 +11,8 @@
             <x-header />
             <x-main.menu.nav />
             <x-main-content/>
+            <x-h1 :title="$title"/>
+            <x-logo src="https://e7.pngegg.com/pngimages/41/701/png-clipart-logo-graphic-design-graphy-grapher-logo-graphy-angle-text.png" width="210" height="210" />
             <p>Это с loyout</p>
             {{$slot}}
             <x-footer.info />
